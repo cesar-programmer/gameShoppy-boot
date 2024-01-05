7f4b8ce6d5b5427181e2a3d7e1f5c6f6afcdacf3
Cesar <CesarCordova.developer@gmail.com>
2024-01-04 16:55:31 -0800

    assignment3

---
+++ scripts/order.js
@@ -0,0 +1,15 @@
+import { games } from './games.js'
+console.log(games)
+
+function init () {
+  const select = document.getElementById('gameTitle')
+  select.innerHTML = '<option value="" disabled selected>Select a category</option>'
+  select.innerHTML += games.map(game => `<option value="${game.title}">${game.title}</option>`).join('')
+}
+
+function orderComplete () {
+  alert('Order completed successfully')
+}
+
+init()
+window.orderComplete = orderComplete
